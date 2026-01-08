482b388bca83636691c4f07dfd8c4552221b176d
vitalica <50740482+vitalica74@users.noreply.github.com>
2026-01-08 17:05:30 +0200

    Update main.py

--- cli/main.py
+++ cli/main.py
@@ -1,1 +1,11 @@
+from frontend.text_processing import tokenize_text
 
+def main():
+    import sys
+    text = " ".join(sys.argv[1:])
+    tokens = tokenize_text(text)
+    print("Tokens:", tokens)
+
+
+if __name__ == "__main__":
+    main()
